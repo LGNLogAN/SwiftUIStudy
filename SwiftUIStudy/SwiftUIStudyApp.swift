c3f8a6a6ba3cb9209660529b9297475259fd9f42
LGNLogAN <logan061175@gmail.com>
2025-11-09 20:24:51 +0900

    [Init] 초기 설정

---
+++ SwiftUIStudy/SwiftUIStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStudyApp.swift
+//  SwiftUIStudy
+//
+//  Created by 최무빈 on 11/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
